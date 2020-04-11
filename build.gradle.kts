66a933f327c42a61155004d58fabb8658cba7583
Piotr Jagielski <pjg@touk.pl>
2020-04-11 23:04:10 +0200

    Merge fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,10 @@ val kotlinVersion: String by project
 
 plugins {
     java
-<<<<<<< HEAD
-<<<<<<< HEAD
     maven
-    `maven-publish`
-    id("com.jfrog.bintray") version "1.8.0"
-=======
->>>>>>> Initial working with Synth and Sample
-=======
-    maven
-<<<<<<< HEAD
     publishing
->>>>>>> Split to packages; Added Config and Application
-=======
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.0"
->>>>>>> Add LFO; Test application
     kotlin("jvm") version("1.3.70")
 }
 
@@ -25,20 +13,11 @@ group = "pl.pjagielski"
 version = "1.0.0-SNAPSHOT"
 
 repositories {
-<<<<<<< HEAD
-<<<<<<< HEAD
     mavenLocal()
-=======
->>>>>>> Initial working with Synth and Sample
-=======
-    mavenLocal()
->>>>>>> Split to packages; Added Config and Application
     mavenCentral()
 }
 
 dependencies {
-<<<<<<< HEAD
-<<<<<<< HEAD
     compile("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.3")
     api("org.jetbrains.kotlin", "kotlin-scripting-common", kotlinVersion)
@@ -52,64 +31,10 @@ dependencies {
     implementation("com.illposed.osc", "javaosc-core", "0.6") {
         exclude("org.slf4j", "slf4j-log4j12")
     }
-=======
-    implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.3")
-    implementation("org.jetbrains.kotlin", "kotlin-scripting-common", kotlinVersion)
-    implementation("org.jetbrains.kotlin", "kotlin-scripting-compiler-embeddable", kotlinVersion)
-    implementation("org.jetbrains.kotlin", "kotlin-scripting-jvm", kotlinVersion)
-    implementation("org.jetbrains.kotlin", "kotlin-scripting-jvm-host-embeddable", kotlinVersion)
-    implementation("org.jetbrains.kotlin", "kotlin-script-runtime", kotlinVersion)
-    implementation("org.jetbrains.kotlin", "kotlin-compiler-embeddable", kotlinVersion)
-    implementation("io.github.microutils", "kotlin-logging", "1.7.8")
-    implementation("com.illposed.osc", "javaosc-core", "0.6")
->>>>>>> Initial working with Synth and Sample
-=======
-    compile("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.3")
-    api("org.jetbrains.kotlin", "kotlin-scripting-common", kotlinVersion)
-    api("org.jetbrains.kotlin", "kotlin-scripting-compiler-embeddable", kotlinVersion)
-    api("org.jetbrains.kotlin", "kotlin-scripting-jvm", kotlinVersion)
-    api("org.jetbrains.kotlin", "kotlin-scripting-jvm-host-embeddable", kotlinVersion)
-    api("org.jetbrains.kotlin", "kotlin-script-runtime", kotlinVersion)
-    api("org.jetbrains.kotlin", "kotlin-compiler-embeddable", kotlinVersion)
-    implementation("io.github.microutils", "kotlin-logging", "1.7.9")
-    compile("com.uchuhimo", "konf", "0.22.1")
-    implementation("com.illposed.osc", "javaosc-core", "0.6") {
-        exclude("org.slf4j", "slf4j-log4j12")
-    }
->>>>>>> Split to packages; Added Config and Application
-
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.0")
     testImplementation("com.willowtreeapps.assertk", "assertk-jvm", "0.21")
 
     runtimeOnly("org.slf4j","slf4j-simple","1.7.29")
-<<<<<<< HEAD
-<<<<<<< HEAD
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/pjagielski/punkt")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
-
-    publications {
-        register("gpr", MavenPublication::class) {
-            from(components["java"])
-        }
-    }
-=======
-    runtimeOnly("org.apache.logging.log4j", "log4j-slf4j-impl", "2.13.0")
->>>>>>> Initial working with Synth and Sample
-=======
->>>>>>> Split to packages; Added Config and Application
 }
 
 publishing {
--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
@@ -15,8 +14,6 @@
 @rem limitations under the License.
 @rem
 
-=======
->>>>>>> Initial working with Synth and Sample
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -33,11 +30,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-<<<<<<< HEAD
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-=======
-set DEFAULT_JVM_OPTS="-Xmx64m"
->>>>>>> Initial working with Synth and Sample
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
